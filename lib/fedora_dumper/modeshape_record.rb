edeaf70e27fa57ab10a9d881e0b8e4abadc301c3
Michael Klein <mbklein@gmail.com>
2018-06-25 09:33:13 -0500

    Add convenience methods to ModeshapeRecord

--- lib/fedora_dumper/modeshape_record.rb
+++ lib/fedora_dumper/modeshape_record.rb
@@ -1,6 +1,22 @@
+require 'recursive-open-struct'
+
 module FedoraDumper
   class ModeshapeRecord < ActiveRecord::Base
     self.table_name = 'modeshape_repository'
+
+    def key
+      to_struct.content.key
+    end
+
+    def name
+      return '' if parent.nil?
+      parent.to_struct.content.children.find { |c| c.key == key }.name
+    end
+
+    def fedora_id
+      return '' if parent.nil?
+      [parent.fedora_id, name].join('/')
+    end
 
     def to_h
       BSON::Document.from_bson(BSON::ByteBuffer.new(content)).to_h
@@ -8,6 +24,16 @@ module FedoraDumper
 
     def to_json
       to_h.to_json
+    end
+
+    def to_struct
+      RecursiveOpenStruct.new(to_h)
+    end
+
+    def parent
+      parent_id = to_struct.content&.parent
+      return nil if parent.nil?
+      self.class.find(parent_id)
     end
   end
 end
